570092fb310ed63c96ae48f1d925c5e7ee592544
MrArigato <121517084+MrArigato@users.noreply.github.com>
2024-07-08 20:21:14 -0400

    Update BookRepository.java

--- bookstore_project/src/main/java/com/group6/bookstore_project/Repository/BookRepository.java
+++ bookstore_project/src/main/java/com/group6/bookstore_project/Repository/BookRepository.java
@@ -3,6 +3,9 @@ package com.brandon.bookSearch.Repository;
 import com.brandon.bookSearch.Entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+import java.util.List;
 
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByIsbn(String isbn);
+    List<Book> findByAuthorId(Long authorId);
 }
