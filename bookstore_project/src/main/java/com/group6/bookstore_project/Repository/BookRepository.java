51d35517a3adb83d965676fb6cc783d73055a17c
MrArigato <121517084+MrArigato@users.noreply.github.com>
2024-07-08 20:40:54 -0400

    Update BookRepository.java

--- bookstore_project/src/main/java/com/group6/bookstore_project/Repository/BookRepository.java
+++ bookstore_project/src/main/java/com/group6/bookstore_project/Repository/BookRepository.java
@@ -8,4 +8,6 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book findByIsbn(String isbn);
     List<Book> findByAuthorId(Long authorId);
+    List<Book> findByGenre(String genre);
+
 }
