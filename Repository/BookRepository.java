c50b16fbeeb4f1b9a4081698a049559645fcd3f5
rdgbrandon <brandonrodguez@gmail.com>
2024-07-24 13:30:30 -0400

    Update BookRepository.java

--- Repository/BookRepository.java
+++ Repository/BookRepository.java
@@ -1,8 +1,17 @@
-package com.group6.bookstore_project.Repository;
+package com.brandon.bookSearch.Repository;
 
-import com.group6.bookstore_project.Model.Book;
+import com.brandon.bookSearch.Entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findByGenre(String genre);
+    @Query("SELECT b FROM Book b ORDER BY b.copiesSold DESC")
+    List<Book> findTop10ByCopiesSold();
 
+    List<Book> findByRatingGreaterThanEqual(double rating);
+
+    List<Book> findByPublisher(String publisher);
 }
