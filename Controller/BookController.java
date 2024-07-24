6f50694520af49627c73db0fdd8c8871f45e9873
rdgbrandon <brandonrodguez@gmail.com>
2024-07-24 13:29:24 -0400

    Update BookController.java

--- Controller/BookController.java
+++ Controller/BookController.java
@@ -1,10 +1,8 @@
-package com.group6.bookstore_project.Controller;
+package com.brandon.bookSearch.Controller;
 
-import com.group6.bookstore_project.service.BookService;
-import com.group6.bookstore_project.Model.Book;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.brandon.bookSearch.Service.BookService;
+import com.brandon.bookSearch.Entity.Book;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +18,22 @@ public class BookController {
     @GetMapping("/allBooks")
     public String getAllBooks() {
         List<Book> books = bookService.getAllBooks();
+        return formatBooks(books);
+    }
+
+    @GetMapping("/getSortByNameAtoZ")
+    public String getSortByNameAtoZ() {
+        List<Book> books = bookService.getAllBooksSortedByTitle();
+        return formatBooks(books);
+    }
+
+    @GetMapping("/genre/{genre}")
+    public String getBooksByGenre(@PathVariable String genre) {
+        List<Book> books = bookService.getBooksByGenre(genre);
+        return formatBooks(books);
+    }
+
+    private String formatBooks(List<Book> books) {
         StringBuilder sb = new StringBuilder();
         sb.append("<html><head><title>Book List</title></head><body><h1>Book List</h1><ul>");
         for (Book book : books) {
@@ -33,17 +47,19 @@ public class BookController {
             sb.append("<li>Description: ").append(book.getBookDesc()).append("</li>");
             sb.append("<li>Publisher: ").append(book.getPublisher()).append("</li>");
             sb.append("<li>Publication Date: ").append(book.getPublicationDate()).append("</li>");
+            sb.append("<li>Rating: ").append(book.getRating()).append("<br></li><br>");
             sb.append("<br><br>");
         }
         sb.append("</ul></body></html>");
         return sb.toString();
     }
 
-    @GetMapping("/getSortByNameAtoZ")
-    public String getSortByNameAtoZ() {
-        List<Book> books = bookService.getAllBooksSortedByTitle();
+    @GetMapping("/topSellers")
+    public String getTopSellers() {
+        List<Book> books = bookService.getTopSellers();
+        int counter = 0;
         StringBuilder sb = new StringBuilder();
-        sb.append("<html><head><title>Sorted Book List</title></head><body><h1>Sorted Book List</h1><ul>");
+        sb.append("<html><head><title>Top Sellers</title></head><body><h1>Top Sellers</h1><ul>");
         for (Book book : books) {
             sb.append("<li><strong>Title: ").append(book.getTitle()).append("</strong></li>");
             sb.append("<li>ID: ").append(book.getBookID()).append("</li>");
@@ -54,10 +70,42 @@ public class BookController {
             sb.append("<li>Price: $").append(book.getPrice()).append("</li>");
             sb.append("<li>Description: ").append(book.getBookDesc()).append("</li>");
             sb.append("<li>Publisher: ").append(book.getPublisher()).append("</li>");
-            sb.append("<li>Publication Date: ").append(book.getPublicationDate()).append("</li>");
+            sb.append("<li>Publication Date: ").append(book.getPublicationDate()).append("</li>");;
+            sb.append("<li>Rating: ").append(book.getRating()).append("<br></li><br>");
             sb.append("<br><br>");
+            counter++;
+            if (counter == 10)
+            {
+              break;
+            }
         }
         sb.append("</ul></body></html>");
         return sb.toString();
+    }
+    @GetMapping("/booksByRating")
+    public String getBooksByRating(@RequestParam double rating) {
+        List<Book> books = bookService.getBooksByRating(rating);
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html><head><title>Books By Rating</title></head><body><h1>Books By Rating</h1><ul>");
+        for (Book book : books) {
+            sb.append("<li><b>Title: ").append(book.getTitle()).append("</b><br>");
+            sb.append("ID: ").append(book.getBookID()).append("<br>");
+            sb.append("Author: ").append(book.getAuthor()).append("<br>");
+            sb.append("Pages: ").append(book.getPages()).append("<br>");
+            sb.append("Genre: ").append(book.getGenre()).append("<br>");
+            sb.append("Copies Sold: ").append(book.getCopiesSold()).append("<br>");
+            sb.append("Price: $").append(book.getPrice()).append("<br>");
+            sb.append("Description: ").append(book.getBookDesc()).append("<br>");
+            sb.append("Publisher: ").append(book.getPublisher()).append("<br>");
+            sb.append("Publication Date: ").append(book.getPublicationDate()).append("<br>");
+            sb.append("Rating: ").append(book.getRating()).append("<br></li><br>");
+        }
+        sb.append("</ul></body></html>");
+        return sb.toString();
+    }
+    @GetMapping("/discountBooksByPublisher")
+    public String discountBooksByPublisher(@RequestParam String publisher, @RequestParam double discountPercentage) {
+        bookService.applyDiscountByPublisher(publisher, discountPercentage);
+        return "Discount applied to books published by " + publisher;
     }
 }
